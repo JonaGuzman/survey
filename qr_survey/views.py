ac16a2b88ee353c386f83a4674a7f1e9372d95f2
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-05-29 07:58:35 -0400

    added index page

--- qr_survey/views.py
+++ qr_survey/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django import template
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('qr_survey/index.html')
+    return HttpResponse(template.render({}, request))
--- survey/settings.py
+++ survey/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qr_survey'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'survey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['qr_survey.templates.qr_survey'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
