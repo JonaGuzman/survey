94d285df2dc2a993a78d820faad669e275d0d349
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-05-29 16:17:40 -0400

    added models poster question and answer

--- qr_survey/models.py
+++ qr_survey/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Poster(models.Model):
+    name = models.CharField(max_length=30, verbose_name='poster name', unique=True)
+    qr_id = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f'{self.name} {self.qr_id}'
+class Question(models.Model):
+    text = models.TextField(max_length=100, verbose_name='questions text')
+
+    def __str__(self):
+        return f'{self.text}'
+class Answer(models.Model):
+    question = models.OneToOneField(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answer_to_question'
+    )
+    value = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+     )
+    
+    def __str__(self):
+        return f'{self.value}'
--- qr_survey/views.py
+++ qr_survey/views.py
@@ -1,8 +1,4 @@
-from django import template
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    template = loader.get_template('qr_survey/index.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'qr_survey/index.html')
