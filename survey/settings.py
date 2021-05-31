158684856561af57520c6d859122304268075ff5
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-05-30 23:15:28 -0400

    migrated db to postgres

--- survey/settings.py
+++ survey/settings.py
@@ -73,11 +73,18 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# For local development: 
+# 'ENGINE': 'django.db.backends.postgresql_psycopg2'
+# 'HOST': 'localhost'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
